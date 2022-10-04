43bebc58490f26bb5fe53861cc3edc5c7f3efd04
LitchDoctor <ogw@halfangle.com>
2022-10-04 09:54:21 -0700

    implement basic server

--- src/main/kotlin/shenanigans/engine/graphics/TextItem.kt
+++ src/main/kotlin/shenanigans/engine/graphics/TextItem.kt
@@ -1,17 +1,30 @@
 package shenanigans.engine.graphics
 
+//TBI: import shenanigans.engine.graphics.Texture
+//TBI: import shenanigans.engine.graphics.Material
 
-class TextItem constructor(private val text : String, fontFileName: String, final val numCols : Int, final val numRows : Int) /*: GameItem*/{
+class TextItem constructor(private val text : String, fontFileName: String, final val numCols : Int, final val numRows : Int) /*: GameItem*/{//needs to super() gameItem when made
 
     private val VERTICES_PER_QUAD = 4
     //private val numCols : Int = col
     //private val numRows : Int = row
     //private val text : String = text
 
-    //textures not set up yet
-    //val texture = Texture(fontFileName)
-    //this.setMesh(buidMesh(texture, numCols, numRows))
+    //TBI: val texture = Texture(fontFileName)
+    //TBI: this.setMesh(buidMesh(texture, numCols, numRows))
 
 
 
 }
+
+//fun getText(): String? {
+    //return text
+//}
+
+//fun setText(text: String) {
+    //this.text = text
+    //val texture: Texture = this.getMesh().getMaterial().getTexture()
+    //this.getMesh().deleteBuffers()
+    //this.setMesh(buildMesh(texture, numCols, numRows))
+//}
+
