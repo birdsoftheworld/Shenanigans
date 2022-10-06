15e3790b79ed9c55fed6eb3811e9d733a33b42f7
Max Niederman <max@maxniederman.com>
2022-10-06 09:07:48 -0700

    fix(engine/ecs): implement runSystem correctly

--- src/main/kotlin/shenanigans/engine/ecs/Entities.kt
+++ src/main/kotlin/shenanigans/engine/ecs/Entities.kt
@@ -6,22 +6,31 @@ class Entities constructor(val componentOrder: List<KClass<Component>>) {
     private val entities: MutableList<Array<Component?>> = arrayListOf()
 
     fun runSystem(system: System) {
-        system.run(EntityIterator(componentOrder, entities))
+        val query = system.query().toSet()
+        val orderedQuery = componentOrder.map {
+            if (query.contains(it)) {
+                it
+            } else {
+                null
+            }
+        }
+
+        system.run(EntityIterator(orderedQuery, entities))
     }
 }
 
-private class EntityIterator(val query: Iterable<KClass<Component>>, val components: List<Array<Component?>>) :
+private class EntityIterator(val query: Iterable<KClass<Component>?>, val entities: List<Array<Component?>>) :
     AbstractIterator<Array<Component?>>() {
     var pos: Int = 0
 
     override fun computeNext() {
-        while (pos < components.size) {
-            val component = components[pos]
+        while (pos < entities.size) {
+            val entity = entities[pos]
 
-            if (query.zip(component).all {
-                it.first.isInstance(it.second)
-            }) {
-                setNext(component.toList().toTypedArray())
+            if (query.zip(entity).all {
+                    it.first !== null && it.first!!.isInstance(it.second)
+                }) {
+                setNext(entity.toList().toTypedArray())
                 break
             } else {
                 pos++
