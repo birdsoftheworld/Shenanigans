81719dd1447a0438c43c90de8ab0301ebd370f75
Max Niederman <max@maxniederman.com>
2022-10-18 09:22:01 -0700

    feat(engine/ecs): add entity versioning

--- src/main/kotlin/shenanigans/engine/ecs/Entities.kt
+++ src/main/kotlin/shenanigans/engine/ecs/Entities.kt
@@ -3,7 +3,8 @@ package shenanigans.engine.ecs
 import kotlin.reflect.KClass
 
 class Entities {
-    private val entities: MutableList<Map<KClass<out Component>, Component>> = arrayListOf()
+    private val entities: MutableList<StoredEntity> = arrayListOf()
+
 
     fun runSystem(system: System) {
         val query = system.query().toSet()
@@ -14,21 +15,36 @@ class Entities {
                 .asSequence()
                 .withIndex()
                 .filter {
-                    it.value.keys.containsAll(query)
+                    it.value.components.keys.containsAll(query)
                 }.map { EntityView(it.index, it.value) },
             lifecycle
         )
         lifecycle.finish(entities)
     }
+
 }
 
-class EntityView internal constructor(val id: Int, val _map: Map<KClass<out Component>, Component>) {
+internal data class StoredEntity(var version: Int, val components: Map<KClass<out Component>, Component>)
+
+class EntityView internal constructor(
+    val id: Int,
+    private val entity: StoredEntity,
+
+    val _components: Map<KClass<out Component>, Component> = entity.components,
+) {
     inline fun <reified T : Component> component(): T {
         return componentOpt()!!
     }
 
-    inline fun <reified T: Component> componentOpt(): T? {
-        return _map[T::class] as T?
+    inline fun <reified T : Component> componentOpt(): T? {
+        return _components[T::class] as T?
+    }
+
+    val version: Int
+        get() = entity.version
+
+    fun mutate() {
+        entity.version++
     }
 }
 
@@ -48,11 +64,11 @@ class EntitiesLifecycle internal constructor() {
         requests.add(LifecycleRequest.Del(id))
     }
 
-    internal fun finish(entities: MutableList<Map<KClass<out Component>, Component>>) {
+    internal fun finish(entities: MutableList<StoredEntity>) {
         requests.forEach { req ->
             when (req) {
                 is LifecycleRequest.Add -> {
-                    entities.add(req.components.associateBy { it::class })
+                    entities.add(StoredEntity(0, req.components.associateBy { it::class }))
                 }
 
                 is LifecycleRequest.Del -> {
