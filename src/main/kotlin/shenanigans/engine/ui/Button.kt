20c61cf85fc6946e4fd695877e20c117c1d73a42
sgagnon0104 <gagnon.silas@gmail.com>
2022-11-17 09:14:13 -0800

    refactor(shape): create isPointInside shape

--- src/main/kotlin/shenanigans/engine/ui/Button.kt
+++ src/main/kotlin/shenanigans/engine/ui/Button.kt
@@ -9,6 +9,7 @@ import shenanigans.engine.events.Event
 import shenanigans.engine.events.EventQueue
 import shenanigans.engine.graphics.api.component.Shape
 import shenanigans.engine.util.Transform
+import shenanigans.engine.util.isPointInside
 import shenanigans.engine.util.setToTransform
 import shenanigans.engine.window.MouseButtonAction
 import shenanigans.engine.window.events.MouseButtonEvent
@@ -30,42 +31,11 @@ class ButtonSystem : System {
                 val mousePos = resources.get<MouseState>().position()
 
                 entities.forEach { button ->
-                    if(pointInside(button.component<Shape>().get(), button.component<Transform>().get(), mousePos)) {
+                    if(button.component<Shape>().get().isPointInside(mousePos, button.component<Transform>().get())) {
                         println("Buttton Pressed!!!!")
                     }
                 }
             }
         }
-    }
-
-    private fun pointInside(shape: Shape, transform: Transform, point: Vector2fc) : Boolean {
-        val transformMatrix = Matrix4f()
-
-        transformMatrix.setToTransform(transform.position, transform.rotation, transform.scale)
-
-        val transformedVertices = Array(shape.vertices.size) { Vector2f() }
-
-        for (i in 0 until shape.vertices.size) {
-            val vertex = Vector4f(shape.vertices[i], 0f, 1f).mul(transformMatrix)
-            transformedVertices[i].x = vertex.x
-            transformedVertices[i].y = vertex.y
-        }
-
-        var count = 0
-        for (i in 0 until shape.vertices.size) {
-            if(pointProjectionIntersectsLine(
-                    point as Vector2f,
-                    Pair(transformedVertices[i], transformedVertices[(i + 1) % shape.vertices.size]))
-            ) {
-                count ++
-            }
-        }
-        return count % 2 == 1
-    }
-
-    private fun pointProjectionIntersectsLine(point: Vector2f, line: Pair<Vector2fc, Vector2fc>) : Boolean {
-        return ((((line.first.y() > point.y() && line.second.y() < point.y()) ||
-            (line.first.y() < point.y() && line.second.y() > point.y()))) &&
-                (point.x() < line.first.x() || point.x() < line.second.x()))
     }
 }
