827a8ad4a009c9b8a03332155442aab74fa37c23
LitchDoctor <ogw@halfangle.com>
2022-12-14 10:02:01 -0800

    good movement placeholder

--- src/main/kotlin/shenanigans/game/player/PlayerController.kt
+++ src/main/kotlin/shenanigans/game/player/PlayerController.kt
@@ -7,6 +7,7 @@ import shenanigans.engine.physics.DeltaTime
 import shenanigans.engine.util.Transform
 import shenanigans.engine.window.Key
 import shenanigans.engine.window.events.KeyboardState
+import kotlin.jvm.internal.Ref.FloatRef
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.sign
@@ -15,17 +16,17 @@ import kotlin.reflect.KClass
 
 data class Player(val xVel: Float,
                   val xAccel: Float = 5f,
-                  val xMax : Float = .2f,
-                  val yMax :  Float = .3f,
-                  val jumpHeight: Float = 50f,
+                  val xMax : Float = .15f,
+                  val jumpHeight: Float = 1f,
                   val jumpSpeed: Float = .2f,
-                  val friction : Float = 5f,
+                  val timeToJump: Float = 1f,
+                  val friction : Float = 8f,
                   val turnSpeed: Float = 20f,
-                  val drag : Float = 6f,
+                  val drag : Float = 8f,
                   var airTurnSpeed : Float = 5f) : Component
 
 class PlayerController : System {
-    val gravity : Float = .0001f;
+    val gravity : Float = .5f;
     var velocity = Vector2f()
 
     override fun query(): Iterable<KClass<out Component>> {
@@ -41,19 +42,22 @@ class PlayerController : System {
             val transform = entity.component<Transform>()
             val pos = transform.get().position
             val xMax = entity.component<Player>().get().xMax
-            val yMax = entity.component<Player>().get().yMax
             val drag = entity.component<Player>().get().drag
             val friction = entity.component<Player>().get().friction
-            var jumpSpeed = entity.component<Player>().get().jumpSpeed
             val turnSpeed = entity.component<Player>().get().turnSpeed
             val airTurnSpeed = entity.component<Player>().get().airTurnSpeed
-            val jumpHeight = entity.component<Player>().get().jumpHeight
-            var desiredVelocity = Vector2f(0f,0f)
-            val onGround = (pos.y == 600f)
+            val jumpHeight: Float = entity.component<Player>().get().jumpHeight
+            val timeToJump = entity.component<Player>().get().timeToJump
+            var desiredVelocity = Vector2f(0f, 0f)
+            val onGround = true
             var deccel = 1f
             val xAccel = entity.component<Player>().get().xAccel
             var turnAccel = 0f
             var maxSpeedChange = 0f
+            var wantToJump = false
+            val jumpSpeed = entity.component<Player>().get().jumpSpeed
+
+
             //left
             if (keyboard.isPressed(Key.A)) {
                 desiredVelocity.add(Vector2f(-xMax, 0f))
@@ -65,61 +69,53 @@ class PlayerController : System {
             }
             //jump
 
-            when(onGround) {
-                true -> {deccel = friction; turnAccel = turnSpeed;if (keyboard.isPressed(Key.W)) {
-                    jumpSpeed = sqrt(-2f * gravity * jumpHeight)
-                    if(velocity.y < 0f){
-//                        jumpSpeed = (jumpSpeed - velocity.y, 0f)
-                    }
-                }}
-                false -> {deccel = drag; turnAccel = airTurnSpeed}
+            when (onGround) {
+                true -> {
+                    deccel = friction; turnAccel = turnSpeed
+                }
+
+                false -> {
+                    deccel = drag; turnAccel = airTurnSpeed
+                }
             }
 
-            if(desiredVelocity.x != 0f) {
+
+            if (keyboard.isPressed(Key.W)) {
+                wantToJump = true
+            }
+
+            fun jump() {
+                velocity.y = -jumpSpeed
+                wantToJump = false
+            }
+
+            if (wantToJump && abs(pos.y-570) < .01) {
+                jump()
+            }
+            if(pos.y < 570){
+                if(velocity.y > 0){
+                    velocity.y += gravity*deltaTime.toFloat()*.5f
+                }
+                velocity.y += gravity*deltaTime.toFloat()
+            }
+
+
+            if (desiredVelocity.x != 0f) {
                 if (desiredVelocity.x.sign != velocity.x.sign) {
                     maxSpeedChange = turnAccel * deltaTime.toFloat()
-                }
-                else {
+                } else {
                     maxSpeedChange = xAccel * deltaTime.toFloat()
                 }
-            }
-            else{
+            } else {
                 maxSpeedChange = deccel * deltaTime.toFloat()
             }
-            velocity.x = velocity.x + (desiredVelocity.x -velocity.x)*maxSpeedChange
-            //println(maxSpeedChange)
+            velocity.x = velocity.x + (desiredVelocity.x - velocity.x) * maxSpeedChange
 
-//            velocity.add(Vector2f(0f,gravity))
-//            //jump
-//            if (keyboard.isPressed(Key.W) && still) {
-//                velocity.y = -jumpSpeed
-//            }
-//            //left
+            pos.add(velocity)
+            println(pos.y)
+            println(velocity.y)
+            transform.mutate()
 
-//            //drag
-//            else{
-//                //friction
-//                if(still && abs(velocity.x) > 0){
-//                    velocity.y = abs(velocity.y)+friction*velocity.y.sign
-//                }//drag
-//                else if(abs(velocity.x) > 0){velocity.y = abs(velocity.y)+drag*velocity.y.sign}
-//
-//                //temporary not falling check
-//                oldPos.y = pos.y
-//            }
-//
-//            if (velocity.length() > 0) {
-//                if(abs(velocity.x) > xMax){
-//                    velocity.x = (xMax*velocity.x.sign)*deltaTime.toFloat()
-//                }
-//
-//                if(abs(velocity.y) > yMax){
-//                    println(velocity.y)
-//                    velocity.y = (yMax*velocity.y.sign)*deltaTime.toFloat()
-//                }
-                pos.add(velocity)
-                transform.mutate()
-
-            }
         }
     }
+}
