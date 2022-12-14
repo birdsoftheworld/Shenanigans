54e6af4c9d7f7632d115fff48ebec06d611b0546
Jasmine <exoticbirdsoftheworld@gmail.com>
2022-12-14 10:01:31 -0800

    feat(resources): mutable resource views

--- src/main/kotlin/shenanigans/engine/ecs/Resource.kt
+++ src/main/kotlin/shenanigans/engine/ecs/Resource.kt
@@ -4,10 +4,33 @@ import kotlin.reflect.KClass
 
 interface Resource
 
+class MutableResourcesView<T>(protected val map: LinkedHashMap<T, Resources>) : ResourcesView(*map.values.toTypedArray()) {
+    /**
+     * set an existing resource to the new value
+     * @throws NoSuchElementException if there is no existing resource matching the given resource
+     */
+    inline fun <reified U : Resource> setExisting(value: U) {
+        getContainerByResourceClass(U::class).set(value)
+    }
+
+    @PublishedApi
+    internal fun getContainerByResourceClass(c: KClass<*>) : Resources {
+        return children.first { it.resources[c] != null }
+    }
+
+    /**
+     * set a new or existing resource at the given target domain
+     * @throws IllegalArgumentException if the domain doesn't exist
+     */
+    fun setAt(value: Resource, target: T) {
+        requireNotNull(map[target]).set(value)
+    }
+}
+
 /**
  * an ordered read-only view of several sets of resources
  */
-class ResourcesView(private vararg val children: Resources) {
+open class ResourcesView(protected vararg val children: Resources) {
     inline fun <reified T : Resource> get(): T {
         return getOpt()!!
     }
