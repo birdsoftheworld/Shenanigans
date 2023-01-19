0a57f2d3c8e56b62af4f17a60cd3c3fc9b317f81
Max Niederman <max@maxniederman.com>
2023-01-19 08:43:16 -0800

    refactor(ui/dsl): prevent builders from overriding children builders

--- src/main/kotlin/shenanigans/engine/ui/dsl/UIBuilder.kt
+++ src/main/kotlin/shenanigans/engine/ui/dsl/UIBuilder.kt
@@ -8,25 +8,25 @@ sealed interface UIBuilder<out T> {
 
 sealed interface ParentUIBuilder {
     fun addChild(child: UIBuilder<Box>)
+}
 
-    fun box(init: BoxBuilder.() -> Unit) {
-        addChild(BoxBuilder().apply(init))
-    }
+fun ParentUIBuilder.box(init: BoxBuilder.() -> Unit) {
+    addChild(BoxBuilder().apply(init))
+}
 
-    fun coloredBox(init: ColoredBoxBuilder.() -> Unit) {
-        addChild(ColoredBoxBuilder().apply(init))
-    }
+fun ParentUIBuilder.coloredBox(init: ColoredBoxBuilder.() -> Unit) {
+    addChild(ColoredBoxBuilder().apply(init))
+}
 
-    fun text(init: TextBuilder.() -> Unit) {
-        addChild(TextBuilder().apply(init))
-    }
+fun ParentUIBuilder.text(init: TextBuilder.() -> Unit) {
+    addChild(TextBuilder().apply(init))
+}
 
-    fun text(text: String, init: TextBuilder.() -> Unit = {}) {
-        addChild(TextBuilder().apply {
-            this.text = text
-            init()
-        })
-    }
+fun ParentUIBuilder.text(text: String, init: TextBuilder.() -> Unit = {}) {
+    addChild(TextBuilder().apply {
+        this.text = text
+        init()
+    })
 }
 
 fun buildUI(init: BoxBuilder.() -> Unit): Box {
