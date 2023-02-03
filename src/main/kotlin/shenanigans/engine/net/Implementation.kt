35136b1e51199426cd9e2abee63b632e722a8bad
Max Niederman <max@maxniederman.com>
2023-02-03 08:43:25 -0800

    feat(net/impl): add client implementation

--- src/main/kotlin/shenanigans/engine/net/Implementation.kt
+++ src/main/kotlin/shenanigans/engine/net/Implementation.kt
@@ -3,6 +3,7 @@ package shenanigans.engine.net
 import com.esotericsoftware.kryonet.Connection
 import com.esotericsoftware.kryonet.Listener
 import com.esotericsoftware.kryonet.Server as KryoServer
+import com.esotericsoftware.kryonet.Client as KryoClient
 
 
 interface NetworkImplementation {
@@ -20,6 +21,25 @@ class Server(private val kryoServer: KryoServer) : NetworkImplementation {
 
     override fun registerListener(listener: (Message) -> Unit) {
         kryoServer.addListener(object : Listener {
+            override fun received(connection: Connection?, `object`: Any?) {
+                if (`object` is Message) {
+                    listener(`object`)
+                }
+            }
+        })
+    }
+}
+
+class Client(private val kryoClient: KryoClient) : NetworkImplementation {
+    override fun sendMessage(msg: Message) {
+        when (msg.delivery) {
+            MessageDelivery.UnreliableUnordered -> kryoClient.sendUDP(msg)
+            MessageDelivery.ReliableOrdered -> kryoClient.sendTCP(msg)
+        }
+    }
+
+    override fun registerListener(listener: (Message) -> Unit) {
+        kryoClient.addListener(object : Listener {
             override fun received(connection: Connection?, `object`: Any?) {
                 if (`object` is Message) {
                     listener(`object`)
